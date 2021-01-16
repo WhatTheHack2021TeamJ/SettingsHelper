dfb5a38f25c04fdbec61eda24787665543e618ee
Joseph Van Boxtel <joseph.vanboxtel@wsu.edu>
2021-01-16 14:39:00 -0800

    Add sample app

---
+++ SettingsHelperSample/SettingsHelperSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SettingsHelperSample
+//
+//  Created by Joseph Van Boxtel on 1/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SettingsHelperSample/SettingsHelperSample/SettingsHelperSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsHelperSampleApp.swift
+//  SettingsHelperSample
+//
+//  Created by Joseph Van Boxtel on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct SettingsHelperSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
